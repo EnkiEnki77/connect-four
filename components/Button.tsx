82c07baf6ed5ff6cf1d9c768e435143a4d713d6a
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-12-08 00:51:49 -0800

    created responsive static menu page

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,9 @@ type Props = {
 const Button = ({ className, children, route }: Props) => {
   return (
     <button
-      className={`${className} relative cursor-pointer hover:border-darkPurple hover:shadow-darkPurple  px-5  leading-[31px] m uppercase h-[72px] w-full rounded-[20px] border-3 border-black shadow-[0px_10px_0px_#000000]`}
+      className={`${className} relative cursor-pointer hover:border-darkPurple hover:shadow-darkPurple  px-5  
+      leading-[31px] m uppercase h-[72px] w-full rounded-[20px] border-3 border-black shadow-[0px_10px_0px_#000000]
+      max-w-[335px] md:max-w-none `}
     >
       {route && <Link className="absolute w-full h-full" href={route} />}
       {children}
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import playVsPlayer from "../public/assets/player-vs-player.svg";
 const Home: NextPage = () => {
   const cunt = "3";
   return (
-    <div className="min-h-screen bg-purple px-5 flex flex-col justify-center items-center gap-[30px] pb-[100px]">
+    <div className="min-h-screen bg-purple md:bg-darkPurple flex justify-center items-center  pb-[100px]">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -23,30 +23,36 @@ const Home: NextPage = () => {
           rel="stylesheet"
         />
       </Head>
-      <Image
-        className="mb-[50px]"
-        src={logo.src}
-        alt="logo"
-        width={52}
-        height={52}
-      />
-      <Button
-        route="/game"
-        className="bg-yellow flex items-center justify-between text-left"
+      <div
+        className="md:bg-purple pt-[200px]  md:max-w-[480px] w-full md:h-[435px] h-full px-5 md:px-10
+      md:pb-[60px] md:pt-[70px] flex flex-col justify-center items-center gap-[30px] rounded-[40px] border-3 
+      border-black shadow-[0px_10px_0px]"
       >
-        <>
-          play vs player
-          <Image
-            src={playVsPlayer}
-            alt="play vs player"
-            width={82}
-            height={46}
-          />
-        </>
-      </Button>
-      <Button route="/rules" className="bg-white text-left">
-        game rules
-      </Button>
+        <Image
+          className="mb-[50px]"
+          src={logo.src}
+          alt="logo"
+          width={52}
+          height={52}
+        />
+        <Button
+          route="/game"
+          className="bg-yellow flex items-center justify-between text-left"
+        >
+          <>
+            play vs player
+            <Image
+              src={playVsPlayer}
+              alt="play vs player"
+              width={82}
+              height={46}
+            />
+          </>
+        </Button>
+        <Button route="/rules" className="bg-white text-left">
+          game rules
+        </Button>
+      </div>
     </div>
   );
 };
