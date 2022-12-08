592d38ded70de3aa034955c4e707bfdcd175a9c9
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-12-07 23:05:50 -0800

    setup project and created button component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+    },
+    extend: {
+      colors: {
+        black: "#000000",
+        darkPurple: "#5C2DD5",
+        purple: "#7945FF",
+        pink: "#FD6687",
+        yellow: "#FFCE67",
+        white: "#FFFFFF",
+      },
+    },
+    fontFamily: {
+      outfit: ["Outfit", "sans-serif"],
+      grotesk: ["Space Grotesk", "serif"],
+    },
   },
   plugins: [],
-}
+};
