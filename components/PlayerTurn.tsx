12b83b0127fde095fe6470996dc4e828e721abd1
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-12-09 04:13:09 -0800

    playerTurn component colors change based on player

--- components/PlayerTurn.tsx
+++ components/PlayerTurn.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React, { useEffect } from "react";
-import Rectangle from "../public/assets/Rectangle.png";
+import RectangleRed from "../public/assets/RectangleRed.png";
+import RectangleYellow from "../public/assets/Group-11.png";
 
 type Props = {
   timer: number;
@@ -9,14 +10,23 @@ type Props = {
 
 const PlayerTurn = ({ timer, turn }: Props) => {
   console.log(timer);
+  const rectangle = turn === 1 ? RectangleRed : RectangleYellow;
 
   return (
     <div className="absolute bottom-[80px] left-0 right-0 mx-auto z-40 flex justify-center">
-      <Image className="shadow-[0px_10px_0px  ]" src={Rectangle} alt="" />
-      <p className="z-50 text-black absolute xs uppercase top-[45px]">
+      <Image className="shadow-[0px_10px_0px  ]" src={rectangle} alt="" />
+      <p
+        className={`z-50 ${
+          turn === 1 ? "text-white" : "text-black"
+        } absolute xs uppercase top-[45px]`}
+      >
         Player {turn}'s turn
       </p>
-      <h2 className="font-grotesk lg text-black absolute top-[70px]">
+      <h2
+        className={`font-grotesk ${
+          turn === 1 ? "text-white" : "text-black"
+        } lg  absolute top-[70px]`}
+      >
         {timer}s
       </h2>
     </div>
